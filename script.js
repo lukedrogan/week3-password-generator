90989a90646941ee7277ea6742bfe72716bf562c
Luke Drogan <luke@super-wide.com>
2021-03-02 18:25:55 +0000

    Added generatePasswoard Function and variables

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword(){
+  var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+  var numbers = "1234567890";
+  var special = "!@£$%^&*()";
+
+
+
+
+
+
+  return "new password"
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
